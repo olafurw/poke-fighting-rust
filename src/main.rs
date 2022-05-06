ea36ea3c0dafdbebebc545612de8314b98b2250d
Wladimir Palant <fqcgithub@palant.de>
2022-05-06 11:43:14 +0200

    Do not reallocate image on each frame

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ fn main()
 struct Model
 {
     battle: Battle,
+    image: nannou::image::DynamicImage,
 }
 
 fn model(app: &App) -> Model
@@ -26,31 +27,32 @@ fn model(app: &App) -> Model
     app.new_window().size(IMG_SIZE as u32, IMG_SIZE as u32).view(view).build().unwrap();
 
     Model {
-        battle: Battle::new()
+        battle: Battle::new(),
+        image: nannou::image::DynamicImage::ImageRgb8(nannou::image::RgbImage::new(IMG_SIZE as u32, IMG_SIZE as u32)),
     }
 }
 
 fn update(_app: &App, model: &mut Model, _update: Update)
 {
     model.battle.action();
+
+    if let nannou::image::DynamicImage::ImageRgb8(ref mut pixels) = model.image
+    {
+        for y in 0..IMG_SIZE
+        {
+            for x in 0..IMG_SIZE
+            {
+                let pokemon = &model.battle.pokemons[y][x];
+                let color: [u8; 3] = pokemon.kind.into();
+                pixels.put_pixel(x as u32, y as u32, nannou::image::Rgb(color));
+            }
+        }
+    }
 }
 
 fn view(app: &App, model: &Model, frame: Frame)
 {
-    let mut pixels = nannou::image::RgbImage::new(IMG_SIZE as u32, IMG_SIZE as u32);
-
-    for y in 0..IMG_SIZE
-    {
-        for x in 0..IMG_SIZE
-        {
-            let pokemon = &model.battle.pokemons[y][x];
-            let color: [u8; 3] = pokemon.kind.into();
-            pixels.put_pixel(x as u32, y as u32, nannou::image::Rgb(color));
-        }
-    }
-
-    let image = nannou::image::DynamicImage::ImageRgb8(pixels);
-    let texture = wgpu::Texture::from_image(app, &image);
+    let texture = wgpu::Texture::from_image(app, &model.image);
 
     frame.clear(PURPLE);
 
